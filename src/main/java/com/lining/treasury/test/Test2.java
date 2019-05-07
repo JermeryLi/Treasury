414e81c8ec0d1efd643fd7836058dee9e54faec7
lining <liningjer@163.com>
2019-05-07 16:19:07 +0800

    hello

---
+++ src/main/java/com/lining/treasury/test/Test2.java
@@ -0,0 +1,12 @@
+package com.lining.treasury.test;
+
+/**
+ * @author lining
+ * @date 2019/5/7
+ * @description Test2
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
